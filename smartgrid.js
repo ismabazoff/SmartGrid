def7f644f1546a78f10be4dd37e4f9298f6dd747
Ismail Abazov <41200208+ismabazoff@users.noreply.github.com>
2024-02-28 20:40:29 +0300

    Add files via upload

---
+++ smartgrid.js
@@ -0,0 +1,10 @@
+import smartGrid from 'smart-grid'
+
+let options = {
+    /* our settings here */
+}
+
+smartGrid('path-to-your-folder', options)
+
+// 1) npm i smart-grid
+// 2) сформировать сетку с помощью команды node [filename]
